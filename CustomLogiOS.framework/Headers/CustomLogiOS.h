7de6d5acc75615e9735ee7454ab04afb7b48a599
Mohammad Zulqurnain <mohammad.zulqurnain@gmail.com>
2019-07-16 00:57:16 +0500

    Created CustomLog framework

---
+++ CustomLogiOS.framework/Headers/CustomLogiOS.h
@@ -0,0 +1,19 @@
+//
+//  CustomLogiOS.h
+//  CustomLogiOS
+//
+//  Created by trial on 7/11/19.
+//  Copyright © 2019 trial. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomLogiOS.
+FOUNDATION_EXPORT double CustomLogiOSVersionNumber;
+
+//! Project version string for CustomLogiOS.
+FOUNDATION_EXPORT const unsigned char CustomLogiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomLogiOS/PublicHeader.h>
+
+
